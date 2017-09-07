898887fa1862fd99706f9c57c165009a5b77a2de
lwio <443474713@qq.com>
2017-09-07 11:47:09 +0800

    simplily add chat style

--- src/components/Chat/gameChat.vue
+++ src/components/Chat/gameChat.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="game-chat">
-    <header class="moveable" v-moveable="'.game-chat'"></header>
+    <header class="moveable" v-moveable="'.game-chat'">
+      <h1>Game Chat</h1>
+    </header>
     <div class="chat-window"></div>
     <div class="chat-input">
       <input>
-      <button></button>
+      <button>Send</button>
     </div>
   </div>
 </template>
@@ -31,24 +33,41 @@ export default {
 
 <style scoped lang="scss">
 .game-chat {
+  display: flex;
+  flex-direction: column;
   width: 300px;
-  height: 300px;
   position: fixed;
   right: 0;
   bottom: 0;
-  background: red;
+  background: transparent;
   header {
-    height: 40px;
-    background: rgba(0, 0, 0, 1.0);
+    flex: 0 0 40px;
+    background: rgba(50, 50, 50, 1.0);
+    h1 {
+      margin: 0;
+      font-size: 12px;
+      color: white;
+    }
   }
   .chat-window {
-
+    flex: 0 0 100px;
+    background: #252830;
   }
   .chat-input {
+    display: flex;
+    align-items: center;
+    flex: 0 0 30px;
+    background: rgba(50, 50, 50, 1.0);
     input {
-
+      width: calc(100% - 50px)
     }
-
+    button {
+      width: 50px;
+    }
   }
+}
+
+.moveable {
+  cursor: move;
 }
 </style>
